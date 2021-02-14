abb0abd9c0a58bb4643fb7d3f629a54848c4bb7a
Pavel <p97p@ya.ru>
2021-02-14 19:28:42 +0300

    implement

--- TestScrollVirtualization/MainWindow.xaml.cs
+++ TestScrollVirtualization/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TestScrollVirtualization
 {
@@ -23,6 +13,14 @@ namespace TestScrollVirtualization
         public MainWindow()
         {
             InitializeComponent();
+            MyButton.Click += (s, e) =>
+            {
+                MyButton.Visibility = Visibility.Collapsed;
+                MyItemsControl.ItemsSource = Enumerable
+                    .Range(0, 10000)
+                    .Select(i => $"Item {i}")
+                    .ToArray();
+            };
         }
     }
 }
